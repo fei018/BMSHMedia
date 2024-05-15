4a8e24cd3104d541f4bedc618a2020e3742c6fe4
fei018 <fei001@outlook.com>
2024-05-15 21:49:27 +0800

    update

---
+++ BMSHMedia.WebClient/Program.cs
@@ -0,0 +1,35 @@
+using BMSHMedia.WebClient.Services;
+using BootstrapBlazor.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using System.Globalization;
+
+namespace BMSHMedia.WebClient
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+            builder.RootComponents.Add<HeadOutlet>("head::after");
+            //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddHttpClient<ApiCallerService>(op => { op.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress); });
+
+            builder.Services.AddBootstrapBlazor(op =>
+            {
+                op.ToastDelay = 4000;
+                op.SupportedCultures = new List<string> { "zh-TW", "zh-CN", "en-US" };
+                op.DefaultCultureInfo = "zh-TW";
+            });
+
+            builder.Services.ConfigureJsonLocalizationOptions(op =>
+            {
+                op.IgnoreLocalizerMissing = true;
+                op.ResourcesPath = "Localization";
+            });
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
---
+++ BMSHMedia.WebClient/Services/ApiCallerService.cs
@@ -0,0 +1,15 @@
+﻿
+namespace BMSHMedia.WebClient.Services
+{
+    public class ApiCallerService
+    {
+        private readonly HttpClient _http;
+
+        public ApiCallerService(HttpClient http)
+        {
+            _http = http;
+        }
+
+
+    }
+}
